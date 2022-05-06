c16148080cc1cfb820205e9f67981000e890fe2d
Sean Vasvari <vasvarisean@gmail.com>
2022-05-06 19:57:58 -0400

    Added parallax background to home screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,20 +4,41 @@ import sun from './Media/sun.png';
 import cloudsForeground from './Media/clouds-foreground.png';
 import cloudsRear from './Media/clouds-rear.png';
 import sunRays from './Media/sun-rays-circle.png';
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    (function () {
+      document.addEventListener("mousemove", parallax);
+      const el = document.querySelector("#logo")
+      console.log(el)
+
+      function parallax(e) {
+        let w = window.innerWidth / 2;
+        let h = window.innerHeight / 2;
+        let mouseX = e.clientX;
+        let mouseY = e.clientY;
+        let depth1 = `${50 - (mouseX - w) * 0.001}% ${50 - (mouseY - h) * 0.001}%`;
+        let depth2 = `${50 - (mouseX - w) * 0.002}% ${50 - (mouseY - h) * 0.002}%`;
+        let depth3 = `${50 - (mouseX - w) * 0.004}% ${50 - (mouseY - h) * 0.004}%`;
+        let depth4 = `${50 - (mouseX - w) * 0.006}% ${50 - (mouseY - h) * 0.006}%`;
+        let x = `${depth4}, ${depth3}, ${depth2}, ${depth1}`;
+        console.log(x);
+        el.style.backgroundPosition = x;
+      }
+    })();
+  }, []);
+
   return (
     <div className="home-main-container">
-        <div className="name-container">
-          <h2>Sean</h2>
-          <h2>Vasvari</h2>
-        </div>
-        <div className="logo-container">
-          <img src={sun} className="sun" alt="logo" />
-          <img src={cloudsForeground} className="clouds-foreground" alt="logo" />
-          <img src={cloudsRear} className="clouds-rear" alt="logo" />
-          <img src={sunRays} className="sun-rays" alt="logo" />
-        </div>
+      <div className="name-container">
+        <h2>Sean</h2>
+        <h2>Vasvari</h2>
+      </div>
+      <div id="logo" className="logo-container">
+        <h2>PORTFOLIO</h2>
+      </div>
     </div>
   );
 }
